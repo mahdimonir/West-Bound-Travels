047d8f65419d1a37fcbf46c7f4b4658c7bb7b0f4
mahdimonir <mahdimoniruzzaman@gmail.com>
2026-01-09 00:19:07 +0600

    Setup environment to deploy in vercel

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -1,12 +1,16 @@
 import pino from 'pino';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
+  ...(isDev && {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+      },
     },
-  },
+  }),
   base: {
     pid: false,
   },
